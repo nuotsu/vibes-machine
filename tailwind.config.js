eca72f1b59b5d12d5c027fad74c9a2e97d330428
Mitchell Christ <xzmaru@gmail.com>
2023-06-04 23:40:06 -0700

    start colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [],
+	content: ['./src/**/*.{html,svelte}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				serif: `'Apple Garamond', serif`,
+			},
+			colors: {
+				sand: '#fdf6e3',
+				wave: '#2664f6',
+				hot: '#ff00a8',
+			},
+			screens: {
+				'<sm': { max: '640px' },
+				'<md': { max: '768px' },
+				'<lg': { max: '1024px' },
+				'<xl': { max: '1280px' },
+				'<2xl': { max: '1536px' },
+			}
+		},
 	},
 	plugins: [],
 }
